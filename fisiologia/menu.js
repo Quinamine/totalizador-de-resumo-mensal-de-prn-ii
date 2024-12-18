f4d6360c7aa579ef14febe6c80ba6c70e83c7027
Adérito Félix Quinamine <quinamine.aderitofelix@gmail.com>
2024-12-18 01:10:44 -0200

    HTML/JS: Actualizei a secção de 'Hiperligações para outros serviços afins' adicionando os links de todas as minhas plataformas disponíveis e criei um método que omite o link da página actual.

--- fisiologia/menu.js
+++ fisiologia/menu.js
@@ -41,7 +41,7 @@ const menu = {
                     }
                 }  
                 if(!numLinhaMatches) {
-                    const msg = `Nenhuma linha corresponde à referência digitada.`;
+                    const msg = `Sem correspondência.`;
                     alertarSobre(msg);
                 }
             },
